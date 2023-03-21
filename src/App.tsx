15dae0e2d5cac4a01c0dbbcc1a000ec7e6d6e16b
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-03-21 10:31:38 -0300

    Implement enun SelectedPage for Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import Navbar from "@/scenes/navbar";
 import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
-
-  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
 
   return (
     <div className="app bg-gray-20">
-      <Navbar 
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
